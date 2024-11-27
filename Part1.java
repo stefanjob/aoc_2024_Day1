cd1e232c314752114009d71cab02b5de7cb64fc3
Stefan Job <stefan.job@me.com>
2024-11-27 21:14:35 +0100

    test

--- Part1.java
+++ Part1.java
@@ -1,5 +1,5 @@
 public class aoc_day1_1 {
     public static void main(String[] args) {
-        System.out.println("AOC Day 1 Part 1");
+        System.out.println("AOC Day 1 Part 1 test");
     }
 }
